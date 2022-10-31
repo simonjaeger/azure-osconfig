dd3693504ff3e8dc069f3a68863fb65bcaaf7188
simonjaeger <simon.jager@live.com>
2022-10-31 10:40:47 +0000

    fix: formatting

--- src/modules/users/ansible/src/lib/ansible-exec.py
+++ src/modules/users/ansible/src/lib/ansible-exec.py
@@ -1,7 +1,10 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 
-import sys, subprocess, json, importlib
+import importlib
+import json
+import subprocess
+import sys
 
 if len(sys.argv) == 3:
     MODULE_NAME = sys.argv[1]
